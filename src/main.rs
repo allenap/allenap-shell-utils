326f4b9e15e66ab71d4325c14ebbb5268fe42ef3
Gavin Panella <gavinpanella@gmail.com>
2025-08-08 23:31:30 +0200

    Actually useful implementation of `watch` command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-use std::{collections, env, error, ffi, path, process};
+// spell-checker: ignore abspath
 
+use std::{
+    collections, env, error, ffi,
+    io::{self, Write},
+    path, process,
+};
+
+use anyhow::{anyhow, Context as _, Result};
 use clap::{command, Parser, Subcommand};
 
 #[derive(Parser)]
@@ -22,18 +29,26 @@ enum Commands {
         path: ffi::OsString,
     },
     /// Watch for changes in files or directories
+    ///
+    /// Events are emitted as JSON, with one object per line. This can be
+    /// readily processed by `jq` or `jaq`, e.g.: `allenap-shell-utils watch . |
+    /// jaq .paths`
     Watch {
         /// The files or directories to watch
         paths: Vec<path::PathBuf>,
     },
 }
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
     process::exit(match cli.command {
         Commands::CleanPath { path } => match clean_path(path) {
             Ok(path) => {
-                println!("{}", path.to_string_lossy());
+                println!(
+                    "{}",
+                    path.into_string()
+                        .map_err(|path| anyhow!("Could not convert path {path:?} to UTF-8"))?
+                );
                 0
             }
             Err(err) => {
@@ -44,28 +59,44 @@ fn main() {
         Commands::Watch { paths } => {
             use notify::Watcher;
 
-            // Create a channel to receive the events.
-            let (tx, rx) = std::sync::mpsc::channel::<notify::Result<notify::Event>>();
+            // Create a channel to receive the events. Sending `None` denotes
+            // the end of the stream; the receiver should not attempt to receive
+            // any more messages.
+            let (tx, rx) = std::sync::mpsc::channel::<Option<notify::Result<notify::Event>>>();
+
+            // Handle Ctrl-C to stop watching.
+            ctrlc::set_handler({
+                let tx = tx.clone();
+                move || tx.send(None).unwrap()
+            })?;
 
             // Create a watcher object, delivering debounced events.
             // The notification back-end is selected based on the platform.
-            let mut watcher: notify::RecommendedWatcher =
-                notify::recommended_watcher(move |res| tx.send(res).unwrap()).unwrap();
-
-            eprintln!("Watcher: {:?}", watcher);
+            let mut watcher: notify::RecommendedWatcher = notify::recommended_watcher(move |res| {
+                tx.send(Some(res)).unwrap_or(()) // Ignore errors when sending to `tx`.
+            })
+            .context("Could not create watcher")?;
 
             // Add a path to be watched. All files and directories at that path and
             // below will be monitored for changes.
-            for path in paths {
+            for path in paths.iter() {
                 watcher
-                    .watch(&path, notify::RecursiveMode::Recursive)
-                    .unwrap();
+                    .watch(path, notify::RecursiveMode::Recursive)
+                    .with_context(|| format!("Could not watch {path:?}"))?
             }
 
+            // Loop over events until asked to stop. Errors from `watcher` are
+            // printed to `stderr` but otherwise ignored.
+            let mut stdout = io::stdout().lock();
             loop {
-                match rx.recv() {
-                    Ok(event) => println!("{:?}", event),
-                    Err(e) => println!("watch error: {:?}", e),
+                match rx.recv()? {
+                    None => break 0, // Stop requested, probably Ctrl-C; exit loop cleanly.
+                    Some(Err(error)) => eprintln!("Error: {error}"),
+                    Some(Ok(event)) => {
+                        serde_json::to_writer(&mut stdout, &event)?;
+                        stdout.write_all(b"\n")?;
+                        stdout.flush()?;
+                    }
                 }
             }
         }
